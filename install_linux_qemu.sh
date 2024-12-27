f4a25370c75cd9698d62dc7379eab0752cf9eda3
Zhenzhong Xu <flyfree@gmail.com>
2024-12-26 22:26:18 -0800

    add additional package installations for linux qemu vms

--- install_linux_qemu.sh
+++ install_linux_qemu.sh
@@ -105,10 +105,28 @@ ln -s $PWD/.gitignore_global $HOME/.gitignore_global
 # install brew
 echo "installing linux brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> ~/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential -y
+
+# install gcc
+brew install gcc
 
 # install conda
 echo "installing miniconda"
 ./install_conda.sh
+
+# install vscoe, gcc
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
 
 # Cleanup
 sudo apt-get autoremove -y
